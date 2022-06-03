a9848a3073e27edb126691fef2d6c53e6d286fb7
Ankur Jain <55fjiwm8@duck.com>
2022-06-03 13:24:03 +0530

    Removed the comment block at bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
- 
+
 #Dictionary for game choices
 gameOptions = {"R":"Rock","P":"Paper","S":"Scissors"}
 
@@ -14,21 +14,21 @@ flag="t"
 userChoice = userInput()
 
 while flag=="t":
-    #"in" operator look for keys in dictionaries and not values. 
+    #"in" operator look for keys in dictionaries and not values.
     while userChoice not in gameOptions:
         print("Error: Not a valid choice")
         userChoice = userInput()
-    
+
     #choose at random from the dict for CPU
     cpuChoice = str(random.choice(list(gameOptions.keys())))
-    
+
     #Get the value corresponding to the key chosen & print them
     print("Player (%s)" % gameOptions.get(userChoice) + " : CPU (%s)" % gameOptions.get(cpuChoice))
 
     #Making a list of user and CPU choices
     userCPUChoice = [userChoice, cpuChoice]
-    
-#The if block below could be optimized    
+
+#The if block below could be optimized
     if userCPUChoice == ["R","S"]:
         print("Player Wins")
         flag="p"
@@ -38,7 +38,7 @@ while flag=="t":
     elif userCPUChoice == ["S","P"]:
         print("Player Wins")
         flag="p"
-    
+
     if userCPUChoice == ["S","R"]:
         print("CPU Wins")
         flag="c"
@@ -48,7 +48,7 @@ while flag=="t":
     elif userCPUChoice == ["P","S"]:
         print("CPU Wins")
         flag="c"
-    
+
     if userCPUChoice == ["S","S"]:
         print("It's a tie. Let's play again")
         flag="t"
@@ -61,13 +61,3 @@ while flag=="t":
         print("It's a tie")
         flag="t"
         userChoice = userInput()
-    
-    #To Zuri Mentor - WHY is the below block not working?
-    # if (userCPUChoice == ["R","S"] or ["P","R"] or ["S","P"]):
-    #     print("Player Wins")
-      
-    # if (userCPUChoice == ["S","R"] or ["R","P"] or ["P","S"]):
-    #     print("CPU Wins")
-    
-    # if (userCPUChoice == ["S","S"] or ["R","R"] or ["P","P"]):
-    #     print("It's a tie")
